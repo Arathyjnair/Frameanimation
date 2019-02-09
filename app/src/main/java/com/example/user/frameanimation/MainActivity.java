eb5ab893121cc6aae3bbdba300c0e444db5d5982
Arathyjnair <praarya11@gmail.com>
2019-02-09 08:54:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/frameanimation/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.frameanimation;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imgvw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         imgvw=(ImageView) findViewById(R.id.heartanim);
+        ((AnimationDrawable)imgvw.getBackground()).start();
+    }
+}
+
+
